00f836e9f221d3984813bb6da5cfc8ef7789463b
valentin fleurit <vfleurit@gmail.com>
2020-12-13 16:11:34 +0100

    some changes in db.js

--- db.js
+++ db.js
@@ -1,6 +1,6 @@
 const low = require('lowdb');
 const FileSync = require('lowdb/adapters/FileSync');
-const adapter = new FileSync("./database/database.json");
+const adapter = new FileSync("./database.json");
 const db = low(adapter);
 
 // default config for db
